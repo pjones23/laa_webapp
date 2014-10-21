e2cd85974a9843240e2600cc950d2efda69181d6
Perron Jones <eaglescb20@yahoo.com>
2014-10-21 16:31:56 -0400

    updated database configuration for codeignitor

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'laa';
+$db['default']['password'] = 'CS6150';
+$db['default']['database'] = 'laadb';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
