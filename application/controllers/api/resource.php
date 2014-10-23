eeeee3282a6297d9c840ef8b507242c76b2ed9cf
Perron Jones <eaglescb20@yahoo.com>
2014-10-23 19:01:30 -0400

    Fixed error with rest routing

--- application/controllers/api/resource.php
+++ application/controllers/api/resource.php
@@ -56,5 +56,60 @@ class Resource extends REST_Controller
             $this->response(NULL, 404);
         }
     }
+
+    function car_get()
+    {
+        if(!$this->get('id'))
+        {
+            $this->response(NULL, 400);
+        }
+ 
+        $user = $this->user_model->get( $this->get('id') );
+         
+        if($user)
+        {
+            $this->response($user, 200); // 200 being the HTTP response code
+        }
+ 
+        else
+        {
+            $this->response(NULL, 404);
+        }
+    }
+     
+    function car_post()
+    {
+        $result = $this->user_model->update( $this->post('id'), array(
+            'name' => $this->post('name'),
+            'email' => $this->post('email')
+        ));
+         
+        if($result === FALSE)
+        {
+            $this->response(array('status' => 'failed'));
+        }
+         
+        else
+        {
+            $this->response(array('status' => 'success'));
+        }
+         
+    }
+     
+    function cars_get()
+    {
+        $users = $this->user_model->get_all();
+         
+        if($users)
+        {
+            $this->response($users, 200);
+        }
+ 
+        else
+        {
+            $this->response(NULL, 404);
+        }
+    }
+
 }
 ?>
